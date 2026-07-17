cb0d1a45ee5044688d6b4ab3d6dd8bed7eb06ed6
Angoty <angotyrabarijaona@gmail.com>
2026-07-17 13:25:22 +0300

    feat: autorisation de frontend en ligne à l'API backend

--- src/main/java/dgi/nifonline/backend/config/CorsConfig.java
+++ src/main/java/dgi/nifonline/backend/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.addAllowedOrigin("https://dgi.nifonline.mg");
+        config.addAllowedOrigin("https://dgi-nifonline-data-quality-frontend.vercel.app");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- src/main/java/dgi/nifonline/backend/config/SecurityConfig.java
+++ src/main/java/dgi/nifonline/backend/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://dgi.nifonline.mg"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://dgi-nifonline-data-quality-frontend.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
