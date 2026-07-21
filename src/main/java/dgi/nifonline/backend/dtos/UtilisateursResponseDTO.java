4d2b7aad43ad6c501fae10063febdd04b0ff4308
Angoty <angotyrabarijaona@gmail.com>
2026-07-21 14:10:42 +0300

    feat: correction DTO UtilisateursResponseDTO pour exposer profil et état avec couleur

--- src/main/java/dgi/nifonline/backend/dtos/UtilisateursResponseDTO.java
+++ src/main/java/dgi/nifonline/backend/dtos/UtilisateursResponseDTO.java
@@ -3,13 +3,14 @@ package dgi.nifonline.backend.dtos;
 import lombok.Getter;
 import lombok.Setter;
 import dgi.nifonline.backend.models.Utilisateur;
+import dgi.nifonline.backend.dtos.ProfilResponseDTO;
 
 @Getter @Setter
 public class UtilisateursResponseDTO {
     private Long id;
     private String email;
     private int etat;
-    private String profil;
+    private ProfilResponseDTO profil;
     private String etatCouleur;
     private String etatIntitule;
 
@@ -19,7 +20,7 @@ public class UtilisateursResponseDTO {
         this.etat = user.getEtat();
         this.etatIntitule = user.getEtat() == 10 ? "actif" : "inactif";
         this.etatCouleur = user.getEtat() == 10 ? "text-green-600" : "text-red-600";
-        this.profil = user.getProfil().getIntitule();
+        this.profil = new ProfilResponseDTO(user.getProfil());
     }
 
 }
