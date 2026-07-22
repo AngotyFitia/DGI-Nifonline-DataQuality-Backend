c3028b85e89970ce63895c85279c2d35391b596d
Angoty <angotyrabarijaona@gmail.com>
2026-07-22 14:06:05 +0300

    fix: correction configuration JPA pour détection des repositories (ProfilRepository, SessionTokenRepository)

--- src/main/java/dgi/nifonline/backend/repositories/ProfilRepository.java
+++ src/main/java/dgi/nifonline/backend/repositories/ProfilRepository.java
@@ -3,7 +3,9 @@ package dgi.nifonline.backend.repositories;
 import dgi.nifonline.backend.models.Profil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfilRepository extends JpaRepository<Profil, Long> {
     Optional<Profil> findByIntitule(String intitule);
 }
--- src/main/java/dgi/nifonline/backend/repositories/SessionTokenRepository.java
+++ src/main/java/dgi/nifonline/backend/repositories/SessionTokenRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import dgi.nifonline.backend.models.SessionToken;
 
+@Repository
 public interface SessionTokenRepository extends JpaRepository<SessionToken, Long> {
     Optional<SessionToken> findByToken(String token);
     void deleteByToken(String token);
