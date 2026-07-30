00bdef7d5a480fcc95e4898c905efbcf1ac8eb47
Angoty <angotyrabarijaona@gmail.com>
2026-07-30 09:17:20 +0300

    feat: ajout du repository ProvinceRepository

---
+++ src/main/java/dgi/nifonline/backend/repositories/ProvinceRepository.java
@@ -0,0 +1,10 @@
+package dgi.nifonline.backend.repositories;
+
+import dgi.nifonline.backend.models.Province;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProvinceRepository extends JpaRepository<Province, Long> {
+    
+}
